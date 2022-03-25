f241efff31cac6a148765ceaae606499ea4828a8
Bogdan Dumitrescu <bogdoomy@gmail.com>
2022-03-25 13:59:33 +0000

    Add cows endpoint

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
+cows = [
+    "moo 1",
+    "moo 2",
+    "moo 3",
+    "moo 4",
+    "moo 5",
+    "moo 6",
+    "moo 7",
+    "moo 8",
+    "moo 9"
+]
 
-@app.route("/")
+@app.route("/cows")
 def hello_world():
-  return "Hello, World!"
+  return jsonify(cows)
