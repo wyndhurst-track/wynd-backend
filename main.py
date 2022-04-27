9043e064fc8126b4b00d16be4576468c3cc076c8
Bogdan Dumitrescu <qy18694@bristol.ac.uk>
2022-04-27 20:55:45 +0100

    Update gunicorn

--- main.py
+++ main.py
@@ -4,8 +4,31 @@ from flask_cors import CORS
 from datetime import datetime, timedelta
 import mysql.connector
 
-app = Flask(__name__)
-CORS(app)
+import logging
+
+log_dir = "/home/qy18694/logs/back-end/"
+log_file = datetime.today().strftime("%Y-%m-%d")
+log_path = log_dir + log_file
+logging.basicConfig(filename=log_path, level=logging.INFO)
+
+def log_info(msg):
+  log(logging.info, msg)
+
+def log(log_fn, msg):
+  # Create the string we want to log
+  log_str = f" {datetime.now()} - {msg}"
+  log_fn(log_str)
+
+log_info("Initialise logging")
+
+def create_app():
+  app = Flask(__name__)
+  CORS(app)
+  log_info("Initialise Flask")
+  return app
+
+if __name__=='main':
+  app = create_app()
 
 db = mysql.connector.connect(
 				host="my-dbs-bp0.bristol.ac.uk",
@@ -14,18 +37,25 @@ db = mysql.connector.connect(
 				port=4407,
 				database="wyndhurstfarm"
 			)
+log_info("Initialise database connection")
   
 cursor = db.cursor(prepared=True)
 
 @app.route("/cows")
 def cows_route():
-  cursor.execute("SELECT cow_ID FROM data")
+  log_info("Receive request: /cows")
+  req_begin = datetime.now()
+  cursor.execute("SELECT DISTINCT cow_ID FROM data ORDER BY cow_ID")
   cows = cursor.fetchall()
+  req_end = datetime.now()
+  timedelta = req_end - req_begin
+  log_info("Request resolve: " + str(timedelta.microseconds / 1000) + " miliseconds")
   return jsonify(cows)
 
 @app.route("/data")
 def data_route():
   cow_header = request.headers.get("cow-list")
+  log_info("Recieve request: /data " + cow_header)
   cow_list = cow_header.strip("[]").split(",")
   max_time = datetime.today().strftime("%Y-%m-%d")
   min_time = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
