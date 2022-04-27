eeb0120caf7e16ce2dbef71ac695e90fafc2c3fc
Bogdan Dumitrescu <bogdoomy@gmail.com>
2022-04-27 15:55:59 +0100

    Add DB connection

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+from datetime import datetime, timedelta
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
 
-cows = [
-    "moo 1",
-    "moo 2",
-    "moo 3",
-    "moo 4",
-    "moo 5",
-    "moo 6",
-    "moo 7",
-    "moo 8",
-    "moo 9"
-]
+db = mysql.connector.connect(
+				host="my-dbs-bp0.bristol.ac.uk",
+				user="qy18694",
+				password="Moocow21extended_",
+				port=4407,
+				database="wyndhurstfarm"
+			)
+  
+cursor = db.cursor(prepared=True)
 
 @app.route("/cows")
-def hello_world():
+def cows_route():
+  cursor.execute("SELECT cow_ID FROM data")
+  cows = cursor.fetchall()
   return jsonify(cows)
+
+@app.route("/data")
+def data_route():
+  cow_header = request.headers.get("cow-list")
+  cow_list = cow_header.strip("[]").split(",")
+  max_time = datetime.today().strftime("%Y-%m-%d")
+  min_time = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+  query_params = cow_list
+  query = "SELECT cx, cy, angle, detect_timestamp FROM data WHERE detect_timestamp BETWEEN '" + str(min_time) + " 00:00:00' AND '" + str(max_time) +" 00:00:00' AND cow_ID IN ({c})".format(c=', '.join(['%s'] * len(cow_list)))
+  cursor.execute(query, query_params)
+  data = cursor.fetchall()
+  return jsonify(data)
